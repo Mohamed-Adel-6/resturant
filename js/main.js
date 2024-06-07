38a4fe95cad397764958743756eddfec9590064f
mohamed adel <135276192+Mohamed-Adel-6@users.noreply.github.com>
2024-06-07 17:13:55 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,11 @@
-
-// global function
-  // function to add and remove active from element
-function addActive(arr){
-  for(let i=0;i<arr.length;i++){
-    arr[i].onclick=()=>{
-      for(let n=0;n<arr.length;n++){
-        arr[n].classList.remove("active");
-      };
-      arr[i].classList.add("active");
-    };
-  };
-};
-function items (item){
-  for(let i=0 ;i<item.length;i++){
-    let box =document.createElement('div');
-    box.className="box-item";
-    let img=document.createElement('img');
-    img.src=`image/${item[i].category}/${item[i].name}.gif`;
-    box.appendChild(img);
-    let title = document.createElement('h3');
-    title.className='title'
-    let titleText = document.createTextNode(item[i].name);
-    title.appendChild(titleText);
-    box.appendChild(title);
-    let pra = document.createElement('p');
-    pra.className='pra';
-    let praText=document.createTextNode(item[i].pra);
-    pra.appendChild(praText);
-    box.appendChild(pra);
-    let price=document.createElement('p');
-    price.className='price';
-    let priceText=document.createTextNode(item[i].price);
-    price.appendChild(priceText);
-    box.appendChild(price);
-    let order=document.createElement('a');
-    order.className='order btn';
-    order.href=""
-    let orderText= document.createTextNode('Order Now');
-    order.appendChild(orderText);
-    box.appendChild(order);
-    boxes.appendChild(box);
-  }
-};
-// nav bar
+// get element
+// navbar
+// let page = document.getElementById('page');
+// let pageOrder = document.getElementById('page-order');
 let bar= document.getElementById("bar");
 let navLinks= document.getElementById("links-icons");
-let navValue=false;
-// array of links
 let links =Array.from(document.querySelectorAll(".link"));
-// show and hide navbar mobile
-bar.onclick=()=>{
-  navValue=!navValue;
-  if(navValue === true){
-    navLinks.style.right="10px";
-    navLinks.style.opacity="1";
-
-    bar.classList.add("active");
-  }else{
-    navLinks.style.right="0";
-    navLinks.style.opacity="0";
-    bar.classList.remove("active");
-  };
-};
-// add active to links of navbar
-addActive(links);
+let navValue=false;
 // signboard
 let signboardImg= document.getElementById("signboard-img");
 let signboardTitle= document.getElementById("signboard-title");
@@ -82,7 +24,192 @@ let signboardElement=[
   offer:"50%",
 }
 ];
+// menu
+let menuBar=Array.from(document.querySelectorAll(".link-menu"));
+let menuContent= document.getElementById('content');
+let burgerBtn=document.getElementById('burger-btn');
+let pizzaBtn=document.getElementById('pizza-btn');
 let signboardStart=0;
+let pizza = [
+  {
+    name:"capriccosa",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"180 LE",
+    category:'pizza'
+  },
+  {
+    name:"carbonara",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"200 LE",
+    category:'pizza'
+  },
+  {
+    name:"dlablo",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"150 LE",
+    category:'pizza'
+  },
+  {
+    name:"margherita",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"210 LE",
+    category:'pizza'
+  },
+  {
+    name:"pepperoni",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"100 LE",
+    category:'pizza'
+  },
+  {
+    name:"prosciutto",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"130 LE",
+    category:'pizza'
+  },
+  {
+    name:"saporita",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"190 LE",
+    category:'pizza'
+  },
+  {
+    name:"vicent",
+    src:".gif",
+    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
+    price:"150 LE",
+    category:'pizza'
+  }
+];
+let burger = [
+  {
+    name:"diablo",
+    src:".png",
+    pra:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"100 LE",
+    category:'burger'
+  },
+  {
+    name:"florence",
+    src:".png",
+    pra:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"250 LE",
+    category:'burger'
+  },
+  {
+    name:"marghrita",
+    src:".png",
+    pra:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"150 LE",
+    category:'burger'
+  },
+  {
+    name:"milano",
+    src:".png",
+    pra:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"200 LE",
+    category:'burger'
+  },
+  {
+    name:"roma",
+    src:".png",
+    pra:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"90 LE",
+    category:'burger'
+  },
+  {
+    name:"venetia",
+    src:".png",
+    pra:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"100 LE",
+    category:'burger'
+  },
+  {
+    name:"verona",
+    src:".png",
+    pra:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"130 LE",
+    category:'burger'
+  },
+  {
+    name:"vincent",
+    src:".png",
+    pra:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ",
+    price:"140 LE",
+    category:'burger'
+  }
+];
+// function to add and remove active from element
+function addActive(arr){
+  for(let i=0;i<arr.length;i++){
+    arr[i].onclick=()=>{
+      for(let n=0;n<arr.length;n++){
+        arr[n].classList.remove("active");
+      };
+      arr[i].classList.add("active");
+    };
+  };
+};
+// function to add item in menu
+function items (item){
+  let mainBox=document.createElement('div');
+  mainBox.id="boxes";
+  for(let i=0 ;i<item.length;i++){
+    let box =document.createElement('div');
+    box.className="box-item";
+    let img=document.createElement('img');
+    img.src=`image/${item[i].category}/${item[i].name}${item[i].src}`;
+    box.appendChild(img);
+    let title = document.createElement('h3');
+    title.className='title'
+    let titleText = document.createTextNode(item[i].name);
+    title.appendChild(titleText);
+    box.appendChild(title);
+    let pra = document.createElement('p');
+    pra.className='pra';
+    let praText=document.createTextNode(item[i].pra);
+    pra.appendChild(praText);
+    box.appendChild(pra);
+    let price=document.createElement('p');
+    price.className='price';
+    let priceText=document.createTextNode(item[i].price);
+    price.appendChild(priceText);
+    box.appendChild(price);
+    let order=document.createElement('a');
+    order.href='#'
+    order.className='order order-now btn';
+    let orderText= document.createTextNode('Order Now');
+    order.appendChild(orderText);
+    box.appendChild(order);
+    mainBox.appendChild(box);
+    menuContent.appendChild(mainBox);
+  }
+};
+// nav bar
+// show and hide navbar mobile
+bar.onclick=()=>{
+  navValue=!navValue;
+  if(navValue === true){
+    navLinks.style.right="10px";
+    navLinks.style.opacity="1";
+
+    bar.classList.add("active");
+  }else{
+    navLinks.style.right="0";
+    navLinks.style.opacity="0";
+    bar.classList.remove("active");
+  };
+};
+// add active to links of navbar
+addActive(links);
+// signboard
 setInterval(()=>{
   signboardImg.src=`image/pizza/${signboardElement[signboardStart].name}.gif`;
   signboardTitle.innerHTML=`${signboardElement[signboardStart].name}`;
@@ -93,68 +220,37 @@ setInterval(()=>{
   }
 },7000)
 // menu
-// let menuBar=Array.from(document.querySelectorAll(".link-menu"));
-// addActive(menuBar);
-let pizza = [
-  {
-    name:"capriccosa",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"180 LE",
-    category:'pizza'
-  },
-  {
-    name:"carbonara",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"200 LE",
-    category:'pizza'
-  },
-  {
-    name:"dlablo",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"150 LE",
-    category:'pizza'
-  },
-  {
-    name:"margherita",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"210 LE",
-    category:'pizza'
-  },
-  {
-    name:"pepperoni",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"100 LE",
-    category:'pizza'
-  },
-  {
-    name:"prosciutto",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"130 LE",
-    category:'pizza'
-  },
-  {
-    name:"saporita",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"190 LE",
-    category:'pizza'
-  },
-  {
-    name:"vicent",
-    pra:"Classic marinara sauce, authentic old-world pepperoni, all-natural...",
-    price:"150 LE",
-    category:'pizza'
-  }
-];
-let boxes= document.getElementById('boxes');
 items(pizza);
-let boxItem=document.getElementsByClassName('box-item');
-// menuBar[1].onclick=()=>{
-//   for(let n = 0 ;n<boxItem.length;n++){
-//     boxItem[0].remove()
-//   }
-// }
-// for(let i =0 ; i<menuBar.length;i++){
-//   menuBar[i].onclick=()=>{
-//     console.log(menuBar[i])
+burgerBtn.onclick=()=>{
+  let boxes =document.getElementById('boxes');
+  boxes.remove();
+  items(burger);
+};
+pizzaBtn.onclick=()=>{
+  let boxes =document.getElementById('boxes');
+  boxes.remove();
+  items(pizza);
+};
+// let ordersBtn =Array.from(document.getElementsByClassName('order-now'));
+// for(let i =0 ;i<ordersBtn.length;i++){
+//   ordersBtn[i].onclick=()=>{
+//     pageOrder.style.display='block';
+//     page.style.display="none";
+//     bar.style.display="none";
+//     let close = document.createElement('div');
+//     close.id='close'
+//     let closeText = document.createTextNode('close');
+//     close.appendChild(closeText)
+//     pageOrder.appendChild(close);
+//     let title = document.createElement('h2');
+//     let titleText = document.createTextNode(pizza[i].name);
+//     title.appendChild(titleText);
+//     pageOrder.appendChild(title);
+//     close.onclick=()=>{
+//       pageOrder.style.display='none';
+//       page.style.display="block";
+//       bar.style.display="block";
+//     }
 //   };
 // };
+console.log('this wep site made by mohamed adel');
